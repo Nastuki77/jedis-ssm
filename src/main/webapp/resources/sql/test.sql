3ff72375aa68cb1f27e1e89fd4f8ec4798823f01
chaiqingliang <chaiql@fruitday.com>
2016-10-13 18:27:56 +0800

    sql

---
+++ src/main/webapp/resources/sql/test.sql
@@ -0,0 +1,54 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2016-10-13 18:27:24
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `permit`
+-- ----------------------------
+DROP TABLE IF EXISTS `permit`;
+CREATE TABLE `permit` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `moduleName` varchar(30) DEFAULT NULL,
+  `moduleValue` varchar(30) DEFAULT NULL,
+  `permitName` varchar(30) DEFAULT NULL,
+  `permitValue` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of permit
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `age` int(3),
+  `password` varchar(32) NOT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `address` varchar(80) DEFAULT NULL,
+  `permissions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('9', 'alice', '0', '123123', 'chaiql@qq.com', '', '');
+INSERT INTO `user` VALUES ('10', 'Nastuki', '0', '08b8ce5e05c3bd07d4d4bb0848fe51ad', 'chaiql@qq.com', '', '');
